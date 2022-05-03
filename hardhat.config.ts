18bc02e75b97c2a4c6800119258d82ede46e74a5
Jared Borders <jlbxsxs@gmail.com>
2022-05-02 22:09:09 -0500

    Add tracer plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+require("hardhat-tracer");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
