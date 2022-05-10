b177dd5ac427eef66ff608ab0b16fae2de6e9b99
Jared Borders <jlbxsxs@gmail.com>
2022-05-10 15:07:08 -0500

    Delete file

--- test/unit/SynthSwap.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-import { ethers, artifacts, waffle } from "hardhat";
-
-describe("SynthSwap", function () {
-    it.only("Test external call", async () => {
-    });
-});
